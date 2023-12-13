19a3921b1fa1dbfb856b2269abe20006f2e45f3b
Gustavo Mendes <gustavormendess@gmail.com>
2023-12-13 18:31:30 -0400

    Add files via upload

---
+++ variaveis/App.java
@@ -0,0 +1,44 @@
+public class App {
+    public static void main(String[] args) {
+        // Variáveis do tipo Inteiro
+        // Byte,short,int,long
+        byte numeroPequeno = 127;
+        short numeroShort = 32767;
+        int numeroMaior = 2147483647;
+        long numeroEnorme = 922337203685477000l;
+
+        System.out.println(numeroPequeno);
+        System.out.println(numeroShort);
+        System.out.println(numeroMaior);
+        System.out.println(numeroEnorme);
+
+        // Variáveis Ponto flutuante
+        // float,double
+        float peso = 78.7f;
+        double pi = 3.1412879312987398127983;
+        
+        System.out.println(peso);
+        System.out.println(pi);
+
+        // Variáveis do Tipo Caractere
+        // char
+        char letra = 'g';
+        char simbolo = '+';
+
+        System.out.println(letra);
+        System.out.println(simbolo);
+
+        // Variáveis do Tipo Booleano
+        // true or false
+        boolean estouProgramando = true;
+
+        System.out.println(estouProgramando);
+
+        // Variáveis do Tipo String
+        String texto = "Sou desenvolvedor";
+        String nome = "Gustavo";
+
+        System.out.println(nome);
+        System.out.println(texto);
+    }
+}
